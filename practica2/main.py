7c7e21755b909a4606a6109d84f370211bb00def
danielduque <danieldqmrt@gmail.com>
2015-06-28 00:49:13 +0200

    Integración completa del localizador de matrículas con el OCR, a falta de corrección de fallos y pruebas

--- practica2/main.py
+++ practica2/main.py
@@ -13,7 +13,7 @@ def AisinsideB((Ax,Ay,Aw,Ah), (Bx,By,Bw,Bh)):
 		return True
 	return False
 
-#calcula el area del rectangulo pasado
+#calcula el area del rectangulo pasado como tupla
 def rectArea ((x,y,w,h)):
 	return w*h
 
@@ -51,94 +51,27 @@ def erosionar (imgthreshold, finalcontours):
 def obtenercaracteristicas(imagen,rectangulos):
 	(x,y,w,h) = rectangulos[0]
 	imagen = imagen[y:y+h,x:x+w]
-	imagen = cv2.resize(imagen, (10,10))
+	imagenres = cv2.resize(imagen, (10,10))
 	#imagen = np.asarray(imagen)
-	imagen = imagen.reshape(1,100)
+	imagen = imagenres.reshape((1,100))
 	return imagen[0]
 
 
 
 #----------------------------Programa principal----------------------------
-numerokeypoints = 100
-orb = cv2.ORB(numerokeypoints,1,1)
-lista = []
+
 pathbase = os.path.dirname(os.path.abspath(__file__))
-pathcars = pathbase + '/testing_ocr/frontal_'
-haarClassifier = pathbase + '/haar/matriculas.xml'
 extension = '.jpg'
-pathchars = pathbase + '/training_ocr/frontal_'
 
-for i in xrange(6, 6):
-	#carga de la imagen
-	imgpath = pathcars + str(i) + extension
-	print imgpath
-	temp = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE) #cv2.CV_LOAD_IMAGE_GRAYSCALE
-	plt.imshow(temp, cmap=mpl.cm.get_cmap('gray'))
-	#plt.imshow(temp)
-	plt.title("Imagen original")
-	plt.show()
-	#lista.append(temp)
+#-----------------------Configuracion del OCR-----------------------
 
-	#umbralizado
-	imgthreshold = cv2.adaptiveThreshold(temp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 7)
-	imgcontours = imgthreshold.copy() #es necesario, luego imgcontours se destruye
-	plt.imshow(imgthreshold, cmap=mpl.cm.get_cmap('gray'))
-	#plt.imshow(imgthreshold)
-	plt.title("Imagen umbralizada")
-	plt.show()
-
-	#deteccion de matricula
-	cascade = cv2.CascadeClassifier(haarClassifier)
-	matriculas = cascade.detectMultiScale(
-		imgthreshold,
-      		scaleFactor=1.1,
-        	minNeighbors=3,
-        	minSize=(5,5),
-        	flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
-	imgthreshold = cv2.cvtColor(imgthreshold, cv2.COLOR_GRAY2BGR) #se transforma para pintar encima en color
-	print matriculas
-	for (x,y,w,h) in matriculas:
-		cv2.rectangle(imgthreshold, (x,y), (x+w, y+h), (255,0,0), 2)
-	plt.imshow(imgthreshold)
-	plt.title("Matricula(s) detectada(s)")
-	plt.show()
-
-
-	#busqueda de contornos
-	contours,hierarchy = cv2.findContours(imgcontours,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #antes habia 1,2
-	top = len(contours)
-	#proportions = []
-	i = iter(contours)
-	finalcontours = []
-	for cnt in i:
-		#cnt = contours[j]
-		x,y,w,h = cv2.boundingRect(cnt)
-		proportion = h/float(w)
-		#si el rectangulo es vertical
-		if (proportion > 1):
-			for matricula in matriculas:
-				#si esta dentro de alguna de las matriculas encontradas
-				if AisinsideB((x,y,w,h), matricula):
-					#si tiene un tamano minimo
-					if h > 0.5*matricula[3]:
-						finalcontours.append((x,y,w,h))
-						cv2.rectangle(imgthreshold,(x,y),(x+w,y+h),(0,255,0),2)
-			#proportions.append(proportion)
-
-	plt.imshow(imgthreshold)
-	plt.title("Caracteres encontrados")
-	plt.show()
-#termina el for de imagenes
-
-
-
-caracteres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A"]#, "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+caracteres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 pathtraining = pathbase + "/training_ocr/"
 C = [] #matriz de caracteristicas
 E = [] #vector de enteros. Fila i de la matriz C -> caracter caracteres[E[i]]
 pos = -1
 for caracter in caracteres:
-	pos = pos+1
+	pos = caracteres.index(caracter)
 	for i in xrange(1,11):
 		#carga de la imagen
 		imgpath = pathtraining + caracter + "_" + str(i) + extension
@@ -152,9 +85,8 @@ for caracter in caracteres:
 		imgcontours = imgthreshold.copy() 
 		contours,hierarchy = cv2.findContours(imgcontours,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-		i = iter(contours)
 		finalcontours = []
-		for cnt in i:
+		for cnt in contours:
 			x,y,w,h = cv2.boundingRect(cnt)
 			if (h < imgheight-5):
 				finalcontours.append((x,y,w,h))
@@ -189,6 +121,77 @@ clasificador.train(CR, E)
 caracRed = lda.transform([caracteristicas])
 retval, results = clasificador.predict(np.float32(caracRed))
 print retval
+
+
+#-----------------------Localizacion y reconocimiento de matriculas-----------------------
+
+numerokeypoints = 100
+orb = cv2.ORB(numerokeypoints,1,1)
+pathcars = pathbase + '/testing_ocr/frontal_'
+haarClassifier = pathbase + '/haar/matriculas.xml'
+pathchars = pathbase + '/training_ocr/frontal_'
+
+for i in xrange(6, 7):
+	#carga de la imagen
+	imgpath = pathcars + str(i) + extension
+	print imgpath
+	temp = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	plt.imshow(temp, cmap=mpl.cm.get_cmap('gray'))
+	plt.title("Imagen original")
+	plt.show()
+
+	#umbralizado
+	imgthreshold = cv2.adaptiveThreshold(temp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 7)
+	imgcontours = imgthreshold.copy() #es necesario, luego imgcontours se destruye
+
+	#deteccion de matricula
+	cascade = cv2.CascadeClassifier(haarClassifier)
+	matriculas = cascade.detectMultiScale(
+		imgthreshold,
+      		scaleFactor=1.1,
+        	minNeighbors=3,
+        	minSize=(5,5),
+        	flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+
+	#mostrar matriculas sobre el coche
+	imgcolour = imgthreshold.copy()
+	imgcolour = cv2.cvtColor(imgcolour, cv2.COLOR_GRAY2BGR)
+	for (x,y,w,h) in matriculas:
+		cv2.rectangle(imgcolour, (x,y), (x+w, y+h), (255,0,0), 2)
+	plt.imshow(imgcolour)
+	plt.title("Matricula(s) detectada(s)")
+	plt.show()
+
+
+	#busqueda de contornos
+	contours,hierarchy = cv2.findContours(imgcontours,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	finalcontours = []
+	for cnt in contours:
+		x,y,w,h = cv2.boundingRect(cnt)
+		proportion = h/float(w)
+		#si el rectangulo es vertical
+		if (proportion > 1):
+			for matricula in matriculas:
+				#si esta dentro de alguna de las matriculas encontradas
+				if AisinsideB((x,y,w,h), matricula):
+					#si tiene un tamano minimo
+					if h > 0.5*matricula[3]:
+						finalcontours.append((x,y,w,h))
+
+	C = [] #matriz de caracteristicas
+	for rect in finalcontours:
+		carac = obtenercaracteristicas(imgthreshold,[rect])
+		C.append(carac)
+	CR = lda.transform(C)
+	retval, results = clasificador.predict(np.float32(CR))
+	print results
+	plt.imshow(imgthreshold)
+	plt.title("Caracteres encontrados")
+	plt.show()
+
+
+
+
 
 
 
