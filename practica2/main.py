1c74a68f96ac0219257a965a373364b4eba70a9a
danielduque <danieldqmrt@gmail.com>
2015-06-28 14:01:34 +0200

    Detecta y separa varias matrículas, coge los archivos que haya en la carpeta indep. del nombre, etc

--- practica2/main.py
+++ practica2/main.py
@@ -65,14 +65,14 @@ extension = '.jpg'
 
 #-----------------------Configuracion del OCR
 
-caracteres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+caracteres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "ESP"]
 pathtraining = pathbase + "/training_ocr/"
 C = [] #matriz de caracteristicas
 E = [] #vector de enteros. Fila i de la matriz C -> caracter caracteres[E[i]]
 pos = -1
 for caracter in caracteres:
 	pos = caracteres.index(caracter)
-	for i in xrange(1,11):
+	for i in xrange(1,251):
 		#carga de la imagen
 		imgpath = pathtraining + caracter + "_" + str(i) + extension
 		temp = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE)
@@ -128,19 +128,24 @@ haarClassifier = pathbase + '/haar/matriculas.xml'
 pathchars = pathbase + '/training_ocr/frontal_'
 testimages = os.listdir(pathcars)
 testimages.sort()
+#testimages = ["frontal_39.jpg"]#"frontal_10.jpg", "frontal_12.jpg", "frontal_39.jpg", "frontal_46.jpg"]
 
 for img in testimages:
 	#carga de la imagen
 	imgpath = pathcars + "/" + img
 	print imgpath
 	temp = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE) #GRAYSCALE
-	#plt.imshow(temp, cmap=mpl.cm.get_cmap('gray'))
-	#plt.title("Imagen original")
-	#plt.show()
+	'''plt.imshow(temp, cmap=mpl.cm.get_cmap('gray'))
+	plt.title("Imagen original")
+	plt.show()'''
 
 	#umbralizado
 	#imgthreshold = cv2.cvtColor(temp,cv2.COLOR_BGR2GRAY)
 	imgthreshold = cv2.adaptiveThreshold(temp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 7)
+	'''plt.imshow(imgthreshold, cmap=mpl.cm.get_cmap('gray'))
+	plt.title("Imagen umbralizada")
+	plt.show()'''
+
 	imgcontours = imgthreshold.copy() #es necesario, luego imgcontours se destruye
 
 	#deteccion de matricula
@@ -157,36 +162,55 @@ for img in testimages:
 	imgcolour = cv2.cvtColor(imgcolour, cv2.COLOR_GRAY2BGR)
 	for (x,y,w,h) in matriculas:
 		cv2.rectangle(imgcolour, (x,y), (x+w, y+h), (255,0,0), 2)
-	#plt.imshow(imgcolour)
-	#plt.title("Matricula(s) detectada(s)")
-	#plt.show()
+	plt.imshow(imgcolour)
+	plt.title("Matricula(s) detectada(s)")
+	plt.show()
 
 
 	#busqueda de contornos
 	contours,hierarchy = cv2.findContours(imgcontours,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	finalcontours = []
+	for j in xrange(0,len(matriculas)):
+		finalcontours.append([])
 	for cnt in contours:
 		x,y,w,h = cv2.boundingRect(cnt)
 		proportion = h/float(w)
 		#si el rectangulo es vertical
 		if (proportion > 1):
-			for matricula in matriculas:
+			for j in xrange(0,len(matriculas)):
 				#si esta dentro de alguna de las matriculas encontradas
-				if AisinsideB((x,y,w,h), matricula):
+				(mx,my,mw,mh) = matriculas[j]
+				matrcontours = []
+				if AisinsideB((x,y,w,h), (mx,my,mw,mh)):
 					#si tiene un tamano minimo
-					if h > 0.5*matricula[3]:
-						finalcontours.append((x,y,w,h))
+					if h > 0.5*mh:
+						finalcontours[j].append((x,y,w,h))
+
+	#mostrar caracteres sobre el coche
+	imgcolour = imgthreshold.copy()
+	imgcolour = cv2.cvtColor(imgcolour, cv2.COLOR_GRAY2BGR)
+	for contours in finalcontours:
+		for (x,y,w,h) in contours:
+			cv2.rectangle(imgcolour,(x,y),(x+w,y+h),(0,255,0),2)
+	plt.imshow(imgcolour)
+	plt.title("Caracteres detectados")
+	plt.show()
+
+
+
 	if (len(finalcontours)>=1):
-		finalcontours = sorted (finalcontours, key=lambda tup: tup[0])
-		C = [] #matriz de caracteristicas
-		for rect in finalcontours:
-			carac = obtenercaracteristicas(imgthreshold,[rect])
-			C.append(carac)
-		CR = lda.transform(C)
-		retval, results = clasificador.predict(np.float32(CR))
-		matr = ""
-		for char in results:
-			matr = matr + (caracteres[int(char)])
-		print matr
+		for contours in finalcontours:
+			if (len(contours)>=1):
+				contours = sorted (contours, key=lambda tup: tup[0])
+				C = [] #matriz de caracteristicas
+				for rect in contours:
+					carac = obtenercaracteristicas(imgthreshold,[rect])
+					C.append(carac)
+				CR = lda.transform(C)
+				retval, results = clasificador.predict(np.float32(CR))
+				matr = ""
+				for char in results:
+					matr = matr + (caracteres[int(char)])
+				print matr
 	else:
-		print "no se ha reconocido la matricula"
+		print "No se ha reconocido la matricula"
